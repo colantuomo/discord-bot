239ba1f2b7ce3e96df61a0e4a49fea0bfe37cbc6
Denis Slapelis <deniswesleyss@hotmail.com>
2020-04-26 13:13:55 -0300

    Utilizando variaveis de ambiente

--- src/infra/environment.ts
+++ src/infra/environment.ts
@@ -1,11 +1,13 @@
 import EnvironmentModel from '../model/environment.model';
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const environment: EnvironmentModel = {
     prefix: ';',
     googleBaseURL: 'https://www.googleapis.com/youtube/v3',
-    token: '',
-    apiKey: '',
-    authToken: '',
+    token: process.env.TOKEN,
+    apiKey: process.env.APIKEY,
+    authToken: process.env.OAUTHTOKEN
 }
 
 export =  environment;
